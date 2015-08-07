2bcbed4028a66dec7feebafa858cbca2b672c761
Jaehyun David Lee <davidlee1996@gmail.com>
2015-08-07 16:25:14 -0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+	function nextLinkClicked(){
+		var currentActiveImage = $(".image-shown");
+		var nextActiveImage = currentActiveImage.next();
+
+		if(nextActiveImage.length == 0){
+			nextActiveImage = $(".carousel-inner a").first();
+		}
+
+		currentActiveImage.removeClass("image-shown").addClass("image-hidden").css("z-index", -10);
+		nextActiveImage.addClass("image-shown").removeClass("image-hidden").css("z-index", 20);
+		$(".carousel-inner a").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+	}
+
+	$(".nextLink").on("click",function(e){
+		nextLinkClicked();
+		e.preventDefault();
+	});
+
+	$(".previousLink").on("click",function(e){
+
+		var currentActiveImage = $(".image-shown");
+		var nextActiveImage = currentActiveImage.prev();
+
+		if(nextActiveImage.length == 0){
+			nextActiveImage = $(".carousel-inner a").last();
+		}
+
+		currentActiveImage.removeClass("image-shown").addClass("image-hidden").css("z-index", -10);
+		nextActiveImage.addClass("image-shown").removeClass("image-hidden").css("z-index", 20);
+		$(".carousel-inner a").not([currentActiveImage, nextActiveImage]).css("z-index", 1);
+
+		e.preventDefault();
+	});
+
+	setInterval(nextLinkClicked,10000);
+
+});
